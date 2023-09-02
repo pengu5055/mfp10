ac0caf976ca0f39e5a0407c1f0acbb0ef0281f59
Marko Urbanč <urbancmarko1@gmail.com>
2023-09-02 03:32:59 +0200

    feat(FDMSolver): :sparkles: add solve method

--- src.py
+++ src.py
@@ -34,3 +34,38 @@ class FDMSolver():
         self.x = np.linspace(x_range[0], x_range[1], N)
         self.dx = self.x[1] - self.x[0]
         self.dt = self.t_points[1] - self.t_points[0]
+
+    def solve(self) -> np.ndarray:
+        """
+        Solve the PDE.
+        """
+        # Initialize the solution array
+        self.solution = np.zeros((len(self.t), self.N))
+
+        # Set the initial condition
+        self.solution[0] = self.initial_condition(self.x)
+        
+        # Set the boundary conditions
+        self.solution[:, 0] = 0
+        self.solution[:, -1] = 0
+
+        # Set the potential
+        self.V = self.potential(self.x)
+
+        # Set up the matrix
+        self.b = 1j*(self.dt/(2*self.dx**2))
+        self.a = - self.b/2
+        self.d = 1 + self.b + 1j * self.dt/2 * self.V
+
+        self.A = np.diag(self.a * np.ones(self.N-1), -1) + \
+            np.diag(self.d * np.ones(self.N), 0) + \
+            np.diag(self.a * np.ones(self.N-1), 1)
+
+        self.A_inv = np.linalg.inv(self.A)
+        self.A_dagger = np.conj(self.A).T
+
+        # Solve the PDE
+        for i in range(1, len(self.t)):
+            self.solution[i] = self.A_inv @ self.A_dagger @ self.solution[i-1]
+        
+        return self.solution
